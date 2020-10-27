f2aa9d172c06313ad7da2dd8f267f02f714e4e45
Swift Studies <SwiftStudies@users.noreply.github.com>
2020-10-27 09:54:03 -0500

    Added resources for isometric maps

---
+++ Sources/TiledResources/Generic Tiled Project/Tilesets/Isometric.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Isometric" tilewidth="16" tileheight="8" tilecount="32" columns="4">
+ <image source="../Images/Isometric.png" width="64" height="64"/>
+</tileset>
