15f4f8b3e25b31890bd5b17b95d9f5d397ef6f2f
Swift Studies <SwiftStudies@users.noreply.github.com>
2020-10-28 08:12:50 -0500

    Added richer set of test data and moved the "one of everything" map in

---
+++ Sources/TiledResources/Generic Tiled Project/Tilesets/Animation.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Animation" tilewidth="16" tileheight="16" tilecount="16" columns="4">
+ <image source="../Images/Animation.png" width="64" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="20"/>
+   <frame tileid="1" duration="20"/>
+   <frame tileid="2" duration="20"/>
+   <frame tileid="3" duration="20"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="10000"/>
+   <frame tileid="5" duration="1000"/>
+   <frame tileid="6" duration="1000"/>
+   <frame tileid="7" duration="2000"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.25" y="3.25" width="9.75" height="9.75"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="11" duration="1000"/>
+   <frame tileid="8" duration="1000"/>
+   <frame tileid="9" duration="1000"/>
+   <frame tileid="10" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.25" y="3" width="9.625" height="9.75">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="12.75">
+    <polygon points="0,0 5,-9.625 9.875,0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.125" y="3">
+    <polygon points="0,0 9.875,0 4.875,10.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
