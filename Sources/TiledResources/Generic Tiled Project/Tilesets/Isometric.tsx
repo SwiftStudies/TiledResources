ed052d4cbc99122d8848366fd80a112b72965575
Swift Studies <SwiftStudies@users.noreply.github.com>
2020-10-28 05:46:51 -0500

    Made it easier to spot when positiong or rendering is flipped for isometric

--- Sources/TiledResources/Generic Tiled Project/Tilesets/Isometric.tsx
+++ Sources/TiledResources/Generic Tiled Project/Tilesets/Isometric.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="Isometric" tilewidth="16" tileheight="8" tilecount="32" columns="4">
+ <grid orientation="isometric" width="16" height="8"/>
  <image source="../Images/Isometric.png" width="64" height="64"/>
 </tileset>
