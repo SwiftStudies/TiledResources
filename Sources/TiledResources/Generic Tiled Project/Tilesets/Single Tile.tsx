d20475af73fc2867202221a9d11bf8279c14aea6
Swift Studies <SwiftStudies@users.noreply.github.com>
2020-10-29 06:42:29 -0500

    Fixes to test data

--- Sources/TiledResources/Generic Tiled Project/Tilesets/Single Tile.tsx
+++ Sources/TiledResources/Generic Tiled Project/Tilesets/Single Tile.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Single Tile" tilewidth="16" tileheight="16" tilecount="1" columns="0">
+<tileset version="1.4" tiledversion="1.4.2" name="Single Tile" tilewidth="16" tileheight="16" tilecount="1" columns="0" backgroundcolor="#ff00ff">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="16" height="16" source="../Images/A.png"/>
