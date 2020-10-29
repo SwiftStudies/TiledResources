bb00cac9711104971e73bb6a93dfdf024b7a45fe
Swift Studies <SwiftStudies@users.noreply.github.com>
2020-10-29 06:16:17 -0500

    Corrects and extension of test data

--- Sources/TiledResources/Generic Tiled Project/Tilesets/Alphabet.tsx
+++ Sources/TiledResources/Generic Tiled Project/Tilesets/Alphabet.tsx
@@ -1,6 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="Alphabet" tilewidth="16" tileheight="16" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
+ <properties>
+  <property name="filteringMode" value="nearest"/>
+ </properties>
  <tile id="0">
   <image width="16" height="16" source="../Images/A.png"/>
  </tile>
--- Sources/TiledResources/Generic Tiled Project/Tilesets/Top Down [Sheet M-4 S-4].tsx
+++ Sources/TiledResources/Generic Tiled Project/Tilesets/Top Down [Sheet M-4 S-4].tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Top Down [Sheet M-4 S-4]" tilewidth="16" tileheight="16" spacing="4" margin="4" tilecount="9" columns="3">
+<tileset version="1.4" tiledversion="1.4.2" name="Top Down [Sheet M-4 S-4]" tilewidth="16" tileheight="16" spacing="4" margin="4" tilecount="9" columns="3" backgroundcolor="#ff00ff">
  <image source="../Images/2D Top Down.png" width="64" height="64"/>
 </tileset>
